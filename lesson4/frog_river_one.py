3d5de2f57e3576396837ed0e404b14a1c3034850
Ahyeong <aylee618@gmail.com>
2021-02-23 13:32:45 +0900

    lesson 4-1. frog river one

---
+++ lesson4/frog_river_one.py
@@ -0,0 +1,24 @@
+def solution(X, A):
+    memo = []
+    for i in range(X):
+        memo.append(1000000)
+    for i in range(len(A)):
+        print(A[i])
+        if memo[A[i]-1] > i:
+            memo[A[i]-1] = i
+    print(memo)
+    memo.sort()
+    print(memo)
+    if memo[len(memo)-1] == 1000000:
+        return -1
+    return memo[len(memo)-1]
+
+
+if __name__ == '__main__':
+    # A = [1, 3, 1, 4, 2, 3, 5, 4]
+    # A = [1, 2, 3, 4, 5, 6, 7]
+    A = []
+    for i in range(100000):
+        A.append(i+1)
+    X = 100000
+    print(solution(X, A))
